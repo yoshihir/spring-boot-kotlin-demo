1f5e4defe7e9e4b5b30366c7c49199cf7464cd5b
yoshihiro.yoshida <yoshihiro.yoshida@bizreach.co.jp>
2022-12-26 15:34:52 +0900

    feat: qodana test

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework:spring-context:5.3.19")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.springframework.boot:spring-boot-starter-web") // spring-boot-starterをラップしているwebで使う(@RestControllerなど)
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // kotlin用
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // kotlin用
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2") // kotlinでjsonシリアライズ用
     implementation("software.amazon.awssdk:sqs:2.17.165")
 
-    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools") // 開発時のコード変更時の自動再起動とかをしてくれる
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
